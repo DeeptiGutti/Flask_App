a5662fcd8193c1d4fcaebfaa9aaa83d0ad97d9ab
Deepti <deeptigutti@gmial.com>
2022-11-12 08:30:30 -0800

    Stlying the Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './ansswer.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = [
+    {
+      'questiontext': 'How are you doing?',
+      'answers': ['Good', 'Great', 'Awesome!', 'Okay']
+    },
+    {
+      'questiontext': 'What\'s is your favourite color?',
+      'answers': ['Red', 'Blue', 'Green', 'white']
+    },
+    {
+      'questiontext': 'Who is your favourite Singer?',
+      'answers': ['SPB', 'TS', 'AR', 'SG']
+    },
+    {
+      'questiontext': 'What\'s your favourite Sweet',
+      'answers': ['Gamun', 'Baklava', 'Pastry', 'Cham Cham']
+    },
+  ];
   var _a = 0;
 
   void _answerQuestions() {
+    //var aBool = true;
+
     setState(() {
       _a++;
     });
@@ -23,40 +44,23 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s is your favourite color?',
-      'Who is your favourite actor?',
-      'What\'s your favourite Sweert',
-      'How are you doing?',
-      'Bye!'
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Personal Quiz"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_a]),
-            ElevatedButton(
-              child: Text("1"),
-              onPressed: _answerQuestions,
-            ),
-            ElevatedButton(
-              child: Text("2"),
-              onPressed: _answerQuestions,
-            ),
-            ElevatedButton(
-              child: Text('3'),
-              onPressed: _answerQuestions,
-            ),
-            ElevatedButton(
-              child: Text('4'),
-              onPressed: _answerQuestions,
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Personal Quiz"),
+          ),
+          body: _a < questions.length
+              ? Column(
+                  children: <Widget>[
+                    Question(questions[_a]['questiontext']),
+                    ...(questions[_a]['answers'] as List<String>).map((answer) {
+                      return Answer(_answerQuestions, answer);
+                    }).toList()
+                  ],
+                )
+              : Center(
+                  child: Text('Done!'),
+                )),
     );
   }
 }
